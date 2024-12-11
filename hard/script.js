62c68bc658a1d2e3c2f872526473049615f06d71
pcatalinahe <hperezcatalina@gmail.com>
2024-12-11 07:27:55 -0500

    added changes

--- hard/script.js
+++ hard/script.js
@@ -1,6 +1,41 @@
-function sharePizza(total, eachPerson) {
-    let total = undefined;
-    let eachPerson = undefined;
+//create outer function
+function information() {
+  //create inner function
+  function closure() {
+    //create object with 2 properties
+    let PII = {
+      name: "Chris",
+      ssn: "123-45-6789",
+    };
 
-    console.log("Each person gets " + eachPerson + "slices of pizza; from our " + total + "slice pizza")
+    function getName() {
+      return PII.name;
+    }
+    //function to keep SSN private
+    function getSSN() {
+      return PII.ssn;
+    }
+
+    return {
+      getName: getName,
+      getSSN: getSSN
+    };
+  }
+  return closure();
 }
+
+// Call the function
+
+const patient2 = information();
+// console.log(patient2.getSSN());
+// console.log(patient2.getName());
+
+// Output: Undefined
+console.log(patient2.names); 
+// Output: Undefined
+  console.log(patient2.ssn); 
+// Output: John
+  console.log(patient2.getName());
+// Output: 123-45-6789
+  console.log(patient2.getSSN()); 
+
